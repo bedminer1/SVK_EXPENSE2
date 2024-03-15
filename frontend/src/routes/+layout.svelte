b27c4e57b90f6841b825e990adbef844f0e63dcb
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-15 16:53:59 +0800

    modal and toast for dashboard page

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { modalRegistry } from '$lib/components/modals/modalRegistry';
 	import '../app.postcss';
 
 	// Highlight JS
@@ -18,10 +19,15 @@
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
+	import { Modal, Toast, storePopup } from '@skeletonlabs/skeleton';
+	import { initializeStores } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	initializeStores()
 </script>
 
+
+<Toast position="br" zIndex="z-[999" />
+<Modal components={modalRegistry} zIndex="z-[888]" />
 <div class="h-[90vh] w-full">
 	<slot />
 </div>
