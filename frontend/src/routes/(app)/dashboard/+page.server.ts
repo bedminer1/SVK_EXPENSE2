95dada1c0f83cd07066c095ee6192d1f0b96b085
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-15 17:58:41 +0800

    course 6: markup for displaying expenses with create, update and delete functionality

--- frontend/src/routes/(app)/dashboard/+page.server.ts
+++ frontend/src/routes/(app)/dashboard/+page.server.ts
@@ -1,61 +1,49 @@
 import type {
-  CategoriesRecord,
   CategoriesResponse,
   ExpensesRecord,
   ExpensesResponse,
 } from "$lib/types/pocketbase";
 import { fail } from "@sveltejs/kit";
+import type { PageServerLoad } from './$types';
 
-export const load = async ({ locals }) => {
-  const categories = await locals.pb
-    .collection("categories")
-    .getFullList<CategoriesReponse>();
-  const expensesWithCategory = await locals.pb
-    .collection("expenses2")
-    .getFullList<ExpensesResponse<{ category: CategoriesReponse }>>({
-      expand: "category",
+export const load = (async ({ locals }) => {
+    const categories = await locals.pb.collection('categories').getFullList<CategoriesResponse>();
+    const expensesWithCategory = await locals.pb.collection('expenses2').getFullList<ExpensesResponse<{category: CategoriesResponse}>>({expand: 'category'});
+
+    const expensesWithCategoryAndImages = expensesWithCategory.map((expense) => {
+        let icon = expense.expand?.category?.icon
+        if (icon) {
+            expense.expand!.category!.icon = locals.pb.files.getUrl(expense.expand?.category!, icon, {'thumb': '100x100'});
+        }
+
+        return expense;
     });
 
-  const expensesWithCategoryAndImages = expensesWithCategory.map((expense) => {
-    let icon = expense.expand?.category?.icon;
-    if (icon) {
-      expense.expand!.category!.icon = locals.pb.files.getUrl(
-        expense.expand?.category!,
-        icon,
-        { thumb: "100x100" }
-      );
-    }
-
-    return expense;
-  });
-
-  return {
-    expenses: expensesWithCategoryAndImages,
-    categories,
-  };
-};
+    return {expenses: expensesWithCategoryAndImages, categories};
+}) satisfies PageServerLoad;
 
 export const actions = {
-  create: async ({ locals, request }) => {
+create: async ({ locals, request }) => {
     const data = await request.formData();
-    data.append("currency", "SGD");
+    data.append("currency", "SGD")
 
-    const hasLabel = data.has("label");
-    const hasAmount = data.has("expense");
-    const hasCategory = data.has("category");
+    const hasLabel = data.has('label');
+    const hasAmount = data.has('expense');
+    const hasCategory = data.has('category');
     if (!hasLabel || !hasAmount || !hasCategory) {
-      return fail(400, { hasLabel, hasAmount, hasCategory });
+        return fail(400, {hasLabel, hasAmount, hasCategory})
     }
+    console.log(data)
 
     try {
-      await locals.pb.collection("expenses").create<ExpensesRecord>(data);
-    } catch (e) {
-      console.log(e);
-      return fail(400, { unknown: true });
+        await locals.pb.collection('expenses2').create<ExpensesRecord>(data)
+    } catch(e) {
+        console.log(e);
+        return fail(400, {unknown: true})
     }
 
-    return { success: true, create: true };
-  },
+    return { success: true, create: true }
+},
   update: async ({ locals, request }) => {
     const data = await request.formData();
     data.append("currency", "SGD");
@@ -70,7 +58,7 @@ export const actions = {
 
     try {
       await locals.pb
-        .collection("expenses")
+        .collection("expenses2")
         .update<ExpensesRecord>(data.get("id")!.toString(), data);
     } catch (e) {
       console.log(e);
@@ -90,7 +78,7 @@ export const actions = {
     }
 
     try {
-      await locals.pb.collection("expenses").delete(data.get("id")!.toString());
+      await locals.pb.collection("expenses2").delete(data.get("id")!.toString());
     } catch (e) {
       console.log(e);
       return fail(400, { unknown: true });
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -21,6 +21,7 @@
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { Modal, Toast, storePopup } from '@skeletonlabs/skeleton';
 	import { initializeStores } from '@skeletonlabs/skeleton';
+	
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 	initializeStores()
 </script>
@@ -28,6 +29,5 @@
 
 <Toast position="br" zIndex="z-[999" />
 <Modal components={modalRegistry} zIndex="z-[888]" />
-<div class="h-[90vh] w-full">
-	<slot />
-</div>
+<slot />
+
