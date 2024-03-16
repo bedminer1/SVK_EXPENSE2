606d6670c9fc92ab8da81a21fdc7050f0a5fbb78
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-17 00:14:50 +0800

    fixed bug with AuthModel type incorrectly applied to user

--- frontend/src/routes/auth/(authed)/+layout.server.ts
+++ frontend/src/routes/auth/(authed)/+layout.server.ts
@@ -1,11 +1,11 @@
 import { serializeNonPOJOs } from "$lib/utils";
-import type { AuthModel } from "pocketbase";
+import type { AuthModel } from 'pocketbase';
+import type { LayoutServerLoad } from './$types';
 
 // load in instance of user
-export const load = async ({ locals }) => {
+export const load = (async ({ locals }) => {
     const user = locals.pb.authStore.model
     console.log(user)
-    return {
-        user: serializeNonPOJOs(user) as { user: AuthModel }
-    }
-}
+    return {user: serializeNonPOJOs(user)} as { user: AuthModel }
+
+}) satisfies LayoutServerLoad
