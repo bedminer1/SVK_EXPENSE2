2b68cc985a7885915bb44ac4c63b4e723db2021c
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-17 13:55:11 +0800

    +page.svelte page done up for settings

---
+++ frontend/src/routes/(app)/settings/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import { FileButton } from "@skeletonlabs/skeleton";
+
+    export let data
+
+    let avatarFiles: FileList
+    let email: string = data.user.email
+    let oldPassword: string
+    let password: string
+
+    $: changes = avatarFiles?.length > 0 || email !== data.user.email || oldPassword && password
+</script>
+
+<form class="h-full w-full" method="post" enctype="multipart/form-data" >
+    <div>
+        <label for="avator" class="label">
+            <span>Avatar:</span>
+            <FileButton name="avatar" bind:files={avatarFiles}>Upload</FileButton>
+        </label>
+
+        <label class="label">
+            <span>Email: </span>
+            <input type="email" class="input" placeholder="mail@example.com" bind:value={email}>
+        </label>
+        <label class="label">
+            <span>Old password: </span>
+            <input type="password" class="input" placeholder="*****" bind:value={oldPassword}>
+        </label>
+        <label class="label">
+            <span>Password: </span>
+            <input type="password" class="input" placeholder="*****" bind:value={password}>
+            <input type="hidden" name="passwordConfirm" class="input" placeholder="*****" bind:value={password} />
+        </label>
+    </div>
+    {#if changes}
+        <footer class="fixed left-0 bottom-0 h-16 w-full bg-surface-900 flex flex-row drop-shadow p-2">
+            <button class="ml-auto btn variant-filled-primary" type="submit">Save</button>
+        </footer>
+    {/if}
+</form>
